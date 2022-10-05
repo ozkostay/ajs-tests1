5c6c940cd4071c0479fe668532a87dc3217685a3
ozkostay <ozkostay@mail.ru>
2022-10-05 19:48:33 +0500

    test1

--- src/calkhealth.js
+++ src/calkhealth.js
@@ -1,10 +1,12 @@
 export default function calkhealth(obj) {
   // console.log('888 ', obj.health);
+  let health;
   if (obj.health > 50) {
-    return 'healthy';
+    health = 'healthy';
   } else if (obj.health < 15) {
-    return 'critical';
+    health = 'critical';
   } else {
-    return 'wounded';
+    health = 'wounded';
   }
+  return health;
 }
